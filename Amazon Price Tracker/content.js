6fd28bc157ea1d5389d32a154d7e6fbde76ddf35
Rishit Kumar <95247997+CoderX30@users.noreply.github.com>
2024-06-18 13:50:43 +0530

    Delete Amazon Price Tracker directory
    Deleted the files

--- Amazon Price Tracker/content.js
+++
@@ -1,7 +0,0 @@
-// content.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == 'get_current_url') {
-      sendResponse({url: window.location.href});
-    }
-  });
-  
